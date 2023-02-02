112532ec7d117e22e95bf3f876b23f59feff4705
Robert0101 <rob.tangled@gmail.com>
2023-02-02 14:17:23 -0300

    create library

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
+const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  console.log(`*ORDERS* LISTENING ON PORT ${PORT}.`);
 }
 bootstrap();
