379f30176466ea711f10edbbaa39e2e74a3d22ed
Robert0101 <rob.tangled@gmail.com>
2023-02-03 20:12:41 -0300

    setup PORT from ConfigService

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,9 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
-const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
   await app.listen(PORT);
   console.log(`*ORDERS* LISTENING ON PORT ${PORT}.`);
 }
