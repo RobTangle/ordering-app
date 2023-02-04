fb6bfe43a3109eded0544813c42075811d3d4e86
Robert0101 <rob.tangled@gmail.com>
2023-02-04 10:05:39 -0300

    injectar billingClient y desarrollar createOrder

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,11 +1,18 @@
+import { Inject } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { BILLING_SERVICE } from './constants/services';
 import { CreateOrderRequest } from './dto/create-order.request.dto';
 import { OrdersRepository } from './orders.repository';
 import { Order } from './schemas/order.schema';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async getOrders(): Promise<Order[]> {
     return this.ordersRepository.find({});
@@ -13,6 +20,23 @@ export class OrdersService {
 
   async createOrder(request: CreateOrderRequest): Promise<Order> {
     // create order
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+
+      await lastValueFrom(
+        this.billingClient.emit('order_created', { request }),
+      );
+      await session.commitTransaction();
+      return order;
+    } catch (error) {
+      console.log(
+        '🚀 ~ file: orders.service.ts:25 ~ OrdersService ~ createOrder ~ error',
+        error,
+      );
+
+      await session.abortTransaction();
+    }
     return this.ordersRepository.create(request);
   }
 }
