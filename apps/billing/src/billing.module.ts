188d0f7c932c1eb1c940f78c238c4bb50f34efea
Robert0101 <rob.tangled@gmail.com>
2023-02-03 23:18:12 -0300

    import ConfigModule & RmqModule

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,21 @@
+import { RmqModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RmqModule,
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
