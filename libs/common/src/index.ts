7707250756b06d928da54da45da2eb6781854104
Robert0101 <rob.tangled@gmail.com>
2023-02-03 23:18:21 -0300

    export rmq.module

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,4 @@ export * from './database/database.module';
 export * from './database/abstract.repository';
 export * from './database/abstract.schema';
 export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
